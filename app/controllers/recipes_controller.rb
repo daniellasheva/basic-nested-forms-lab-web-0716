ae0d1f14e84d26862d2159ea5185f2e81ed0076d
daniellasheva <daniellasheva@gmail.com>
2016-08-24 18:50:14 -0400

    initial commit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,26 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    #puts the ingredients into the recipe according to the form
+    10.times {@recipe.ingredients.build}
   end
 
   def create
+    @recipe=Recipe.new(recipe_params)
+    if @recipe.save
+      @recipe=Recipe.create(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
+
+  def recipe_params
+    params.require(:recipe).permit(:title,
+    ingredients_attributes:[
+      :name,
+      :quantity
+      ])
+  end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,16 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients,
+
+  :reject_if => :all_blank
+
+  #proc {|ingredients|ingredient.empty?}
+
+
+  # def ingredients_attributes=(ingredients_attributes)
+  #   ingredients_attributes.values.each do |value|
+  #     unless ingredient[:name].empty?
+  #     self.ingredients<<Ingredient.find_or_create_by(name: ingredient[:name])
+  # end
+
 end
